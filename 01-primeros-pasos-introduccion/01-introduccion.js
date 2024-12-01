df280b4d2f556832ed4fb8dc5f82e53c97ae0e15
Antonio <antonicut@gmail.com>
2024-12-01 12:28:36 +0100

    Add - Seccion 1 - Primeros Pasos e Introduccion - Y crear estructura del proyecto

---
+++ 01-primeros-pasos-introduccion/01-introduccion.js
@@ -0,0 +1,12 @@
+//  **************************************************************************
+//  **********  /01-primeros-pasos-introduccion/01-introduccion.js  **********  
+//  **************************************************************************  
+
+
+$(document).ready(function () {
+    
+    //  ----------  Documento Cargado!!!  ----------
+    console.warn('----------  Documento Cargado!!!  -----  01-introduccion  ----- jQuery version:', $.fn.jquery, ' ----------', '\n');
+
+
+});
---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+//  ******************************************
+//  **********  /assets/js/main.js  **********
+//  ******************************************
+
+
+$(document).ready(function () {
+
+    console.warn('----------  Documento Cargado!!!  ----- jQuery version:', $.fn.jquery, ' ----------', '\n');
+    
+    //  ----------  01-primeros-pasos  ----------
+    $('#primerosPasos').on('click', function () {
+        
+        $("#renderHtml").load('./01-primeros-pasos-introduccion/01-introduccion.html');
+        $.getScript('./01-primeros-pasos-introduccion/01-introduccion.js')
+    });
+
+
+    //  ----------  02-selectores  ----------
+    $('#selectores').on('click', function () {
+        $("#renderHtml").load('./02-selectores/02-selectores.html');
+        $.getScript('./02-selectores/02-selectores.js')
+    });
+
+});
